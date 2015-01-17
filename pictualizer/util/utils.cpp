03d21ede6fcad49a3b95f79e0dd31a67ed4e8f0d
aeroheim <benben123213@gmail.com>
2015-01-16 18:40:31 -0600

    pathIsImage() & pathIsMusic() @ utils

--- pictualizer/util/utils.cpp
+++ pictualizer/util/utils.cpp
@@ -14,4 +14,25 @@ namespace utils
 		char temp[MAXPATHLEN];
 		return str2wstr(_getcwd(temp, MAXPATHLEN) ? std::string(temp) : std::string(""));
 	}
+
+	bool pathIsImage(std::string path)
+	{
+		size_t extensionIndex = path.find_last_of(".", path.length() - 1);
+		std::string extension = path.substr(extensionIndex);
+
+		if (extension.compare(".JPG") || extension.compare(".jpg") || extension.compare(".PNG") || extension.compare(".png")
+			|| extension.compare(".JPEG") || extension.compare(".jpeg"))
+			return true;
+		
+		return false;
+	}
+
+	bool pathIsMusic(std::string path)
+	{
+		size_t extensionIndex = path.find_last_of(".", path.length() - 1);
+		std::string extension = path.substr(extensionIndex);
+
+		// fix this later
+		return false;
+	}
 }
--- pictualizer/util/utils.h
+++ pictualizer/util/utils.h
@@ -9,4 +9,6 @@ namespace utils
 {
 	std::wstring str2wstr(std::string);
 	std::wstring getwstrcwd();
+	bool pathIsImage(std::string);
+	bool pathIsMusic(std::string);
 }
