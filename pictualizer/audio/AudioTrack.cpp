3ad727001992caad82a9cbba3d5daf34ea42c4cb
aeroheim <benben123213@gmail.com>
2015-04-14 16:05:43 -0500

    Album Art support for MP3, MP4, FLAC, ASF, APE, MPC, WAV files. Closes #40

--- pictualizer/audio/AudioTrack.cpp
+++ pictualizer/audio/AudioTrack.cpp
@@ -1,59 +1,215 @@
 #include "AudioTrack.h"
+#include <iostream>
 
-AudioTrack::AudioTrack(std::string path)
+using namespace std;
+
+AudioTrack::AudioTrack(std::string path) : filePath(L""), title(L""), artist(L""), album(L""), duration(0), populated(false)
 {
-	/*
-	 *	FileRef is too simplistic for our eventual needs, which will involve extracting and
-	 *	editing more detailed tags. For now it will suffice, but we should eventually move on to
-	 *	using Taglib's format specific subclasses for handling tags. (TODO)
-	 */
-
 	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-	std::wstring wstrpath = converter.from_bytes(path);
-
-	TagLib::FileRef track(wstrpath.c_str());
-
-	assert(!track.isNull());
-
-	filePath = wstrpath;
-	title = track.tag()->title().toCString();
-	artist = track.tag()->artist().toCString();
-	album = track.tag()->album().toCString();
-
-	// Duration is ignored for now.
-	duration = 0;
+	filePath = converter.from_bytes(path);
 }
 
-AudioTrack::~AudioTrack()
-{
-}
+AudioTrack::~AudioTrack() {}
 
-std::wstring AudioTrack::getPath()
+std::wstring AudioTrack::getPath() const
 {
 	return filePath;
 }
 
-std::string AudioTrack::getTitle()
+std::wstring AudioTrack::getTitle() const
 {
+	if (!populated)
+		populateMetadata();
+
 	return title;
 }
 
-std::string AudioTrack::getArtist()
+std::wstring AudioTrack::getArtist() const
 {
+	if (!populated)
+		populateMetadata();
+
 	return artist;
 }
 
-std::string AudioTrack::getAlbum()
+std::wstring AudioTrack::getAlbum() const
 {
+	if (!populated)
+		populateMetadata();
+
 	return album;
 }
 
-int AudioTrack::getDuration()
+SDL_Texture* AudioTrack::getAlbumArt(SDL_Renderer* ren) const
 {
+	TagLib::FileRef track(getPath().c_str());
+
+	SDL_Texture* albumArt = nullptr;
+
+	if (TagLib::MPEG::File* mpegFile = dynamic_cast<TagLib::MPEG::File*>(track.file()))
+	{
+		if (mpegFile->ID3v2Tag())
+			albumArt = getID3v2AlbumArt(ren, mpegFile->ID3v2Tag());
+		
+		if (mpegFile->APETag() && !albumArt)
+			albumArt = getAPEAlbumArt(ren, mpegFile->APETag());
+	}
+	else if (TagLib::MP4::File* mp4File = dynamic_cast<TagLib::MP4::File*>(track.file()))
+	{
+		if (mp4File->tag())
+			albumArt = getMP4AlbumArt(ren, mp4File->tag());
+	}
+	else if (TagLib::FLAC::File* flacFile = dynamic_cast<TagLib::FLAC::File*>(track.file()))
+	{
+		albumArt = getFLACAlbumArt(ren, flacFile);
+
+		if (!albumArt && flacFile->ID3v2Tag())
+			albumArt = getID3v2AlbumArt(ren, flacFile->ID3v2Tag());
+	}
+	else if (TagLib::ASF::File* asfFile = dynamic_cast<TagLib::ASF::File*>(track.file()))
+	{
+		albumArt = getASFAlbumArt(ren, asfFile->tag());
+	}
+	else if (TagLib::APE::File* apeFile = dynamic_cast<TagLib::APE::File*>(track.file()))
+	{
+		if (apeFile->APETag())
+			albumArt = getAPEAlbumArt(ren, apeFile->APETag());
+	}
+	else if (TagLib::MPC::File* mpcFile = dynamic_cast<TagLib::MPC::File*>(track.file()))
+	{
+		if (mpcFile->APETag())
+			albumArt = getAPEAlbumArt(ren, mpcFile->APETag());
+	}
+	else if (TagLib::WavPack::File* wavFile = dynamic_cast<TagLib::WavPack::File*>(track.file()))
+	{
+		if (wavFile->APETag())
+			albumArt = getAPEAlbumArt(ren, wavFile->APETag());
+	}
+
+	// TODO: Add platform specific directory searching to find album art if not embedded in tags.
+	if (!albumArt)
+	{
+
+	}
+
+	return albumArt;
+}
+
+int AudioTrack::getDuration() const
+{
+	if (!populated)
+		populateMetadata();
+
 	return duration;
 }
 
 bool operator==(AudioTrack& lhs, AudioTrack& rhs)
 {
 	return lhs.filePath == rhs.filePath;
+}
+
+void AudioTrack::populateMetadata() const
+{
+	TagLib::FileRef track(getPath().c_str());
+
+	assert(!populated);
+	assert(!track.isNull());
+
+	title = track.tag()->title().toCWString();
+	artist = track.tag()->artist().toCWString();
+	album = track.tag()->album().toCWString();
+	duration = track.audioProperties()->length();
+
+	populated = true;
+}
+
+SDL_Texture* AudioTrack::getID3v2AlbumArt(SDL_Renderer* ren, TagLib::ID3v2::Tag* tag) const
+{
+	const TagLib::ID3v2::FrameList& frameList = tag->frameList("APIC");
+
+	if (!frameList.isEmpty())
+	{
+		// ID3v2 Album art defaults to front cover.
+		TagLib::ID3v2::AttachedPictureFrame* frame = static_cast<TagLib::ID3v2::AttachedPictureFrame*>(frameList.front());
+		return dataToAlbumArt(ren, frame->picture());
+	}
+
+	return nullptr;
+}
+
+SDL_Texture* AudioTrack::getAPEAlbumArt(SDL_Renderer* ren, TagLib::APE::Tag* tag) const
+{
+	const TagLib::APE::ItemListMap& itemListMap = tag->itemListMap();
+
+	if (itemListMap.contains("COVER ART (FRONT)"))
+	{
+		const TagLib::ByteVector nullStringTerminator(1, 0);
+
+		TagLib::ByteVector item = itemListMap["COVER ART (FRONT)"].value();
+		int position = item.find(nullStringTerminator);
+
+		if (++position > 0)
+			return dataToAlbumArt(ren, item.mid(position));
+	}
+
+	return nullptr;
+}
+
+SDL_Texture* AudioTrack::getMP4AlbumArt(SDL_Renderer* ren, TagLib::MP4::Tag* tag) const
+{
+	if (tag->itemListMap().contains("covr"))
+	{
+		TagLib::MP4::CoverArtList coverArtList = tag->itemListMap()["covr"].toCoverArtList();
+
+		if (coverArtList[0].data().size() > 0)
+			return dataToAlbumArt(ren, coverArtList[0].data());
+	}
+
+	return nullptr;
+}
+
+SDL_Texture* AudioTrack::getFLACAlbumArt(SDL_Renderer* ren, TagLib::FLAC::File* file) const
+{
+	const TagLib::List<TagLib::FLAC::Picture*>& pictureList = file->pictureList();
+
+	// Take the first image, which is usually the front cover.
+	if (!pictureList.isEmpty())
+		return dataToAlbumArt(ren, pictureList[0]->data());
+
+	return nullptr;
+}
+
+SDL_Texture* AudioTrack::getASFAlbumArt(SDL_Renderer* ren, TagLib::ASF::Tag* tag) const
+{
+	const TagLib::ASF::AttributeListMap& attributeListMap = tag->attributeListMap();
+
+	if (attributeListMap.contains("WM/Picture"))
+	{
+		const TagLib::ASF::AttributeList& attributeList = attributeListMap["WM/Picture"];
+
+		if (!attributeList.isEmpty())
+		{
+			// Take the first image, which is usually the front cover.
+			TagLib::ASF::Picture wmpic = attributeList[0].toPicture();
+
+			if (wmpic.isValid())
+				return dataToAlbumArt(ren, wmpic.picture());
+		}
+	}
+
+	return nullptr;
+}
+
+
+SDL_Texture* AudioTrack::dataToAlbumArt(SDL_Renderer* ren, TagLib::ByteVector& data) const
+{
+	if (SDL_RWops* albumArtBuffer = SDL_RWFromMem(data.data(), data.size()))
+	{
+		SDL_Texture* albumArt = IMG_LoadTexture_RW(ren, albumArtBuffer, 0);
+		SDL_SetTextureBlendMode(albumArt, SDL_BLENDMODE_BLEND);
+
+		return albumArt;
+	}
+
+	return nullptr;
 }
--- pictualizer/audio/AudioTrack.h
+++ pictualizer/audio/AudioTrack.h
@@ -1,12 +1,23 @@
 #pragma once
 
-#include "../io/EventObserver.h"
-#include <fileref.h>
+#include <SDL.h>
+#include <SDL_image.h>
 #include <taglib.h>
+#include <fileref.h>
+#include <mpegfile.h>
+#include <flacfile.h>
+#include <mp4file.h>
+#include <asffile.h>
+#include <apefile.h>
+#include <mpcfile.h>
+#include <wavpackfile.h>
+#include <id3v2tag.h>
+#include <apetag.h>
 #include <assert.h>
 #include <string>
 #include <locale>
 #include <codecvt>
+#include "../io/EventObserver.h"
 
 /*
  *	The AudioTrack class serves as the default object representation of tracks in pictualizer.
@@ -19,21 +30,34 @@ class AudioTrack : public EventObserver
 		virtual ~AudioTrack();
 
 		/*
-		 *	AudioTrack metadata should be lazy-initialized in order to maintain reasonable 
-		 *	performance when loading large playlists. Only when a view requests metadata should
-		 *	the respective metadata be retrieved. (TODO)
+		 *	AudioTrack metadata are lazy-initialized in order to maintain reasonable 
+		 *	performance when loading large playlists. The metadata for an audioTrack is
+		 *	populated only when a getter has been called.
 		 */
-		std::wstring getPath();
-		std::string getTitle();
-		std::string getArtist();
-		std::string getAlbum();
-		int getDuration();
+		std::wstring getPath() const;
+		std::wstring getTitle() const;
+		std::wstring getArtist() const;
+		std::wstring getAlbum() const;
+		SDL_Texture* getAlbumArt(SDL_Renderer* ren) const;
+		int getDuration() const;
+
 		friend bool operator== (AudioTrack& lhs, AudioTrack& rhs);
 
 	private:
 		std::wstring filePath;
-		std::string title;
-		std::string artist;
-		std::string album;
-		int duration;
+		mutable std::wstring title;
+		mutable std::wstring artist;
+		mutable std::wstring album;
+		mutable int duration;
+
+		mutable bool populated;
+		void populateMetadata() const;
+
+		SDL_Texture* getID3v2AlbumArt(SDL_Renderer* ren, TagLib::ID3v2::Tag* tag) const;
+		SDL_Texture* getAPEAlbumArt(SDL_Renderer* ren, TagLib::APE::Tag* tag) const;
+		SDL_Texture* getMP4AlbumArt(SDL_Renderer* ren, TagLib::MP4::Tag* tag) const;
+		SDL_Texture* getFLACAlbumArt(SDL_Renderer* ren, TagLib::FLAC::File* file) const;
+		SDL_Texture* getASFAlbumArt(SDL_Renderer* ren, TagLib::ASF::Tag* tag) const;
+
+		SDL_Texture* dataToAlbumArt(SDL_Renderer* ren, TagLib::ByteVector& data) const;
 };
