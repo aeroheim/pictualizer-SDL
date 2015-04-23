bb5fd370e1607b5bdf6a327597f154f913579482
aeroheim <benben123213@gmail.com>
2015-04-22 19:02:10 -0500

    Merged ImageTexture into Image, closes #49

--- pictualizer/controls/Image.cpp
+++ pictualizer/controls/Image.cpp
@@ -1,28 +1,40 @@
 #include "Image.h"
 
-Image::Image(SDL_Texture* image, float x, float y, float w, float h) : 
+Image::Image(ImageTexture image, float x, float y, float w, float h) : 
 	PControl(x, y, w, h),
-	image(image)
+	image(image),
+	loadQueueCount(0)
 {
 	dest.x = getRoundedX();
 	dest.y = getRoundedY();
 	dest.w = getRoundedWidth();
 	dest.h = getRoundedHeight();
+
+	mutex = SDL_CreateMutex();
 }
 
 Image::Image(float x, float y, float w, float h) : Image(nullptr, x, y, w, h) {}
 
 Image::~Image()
 {
-	if (image)
-		SDL_DestroyTexture(image);
+	SDL_UnlockMutex(mutex);
+	SDL_DestroyMutex(mutex);
 }
 
-void Image::setImage(SDL_Texture* image, bool free)
+Image::Image(const Image& other) : PControl(other)
 {
-	if (free && this->image)
-		SDL_DestroyTexture(this->image);
+	image = other.image;
+}
 
+Image& Image::operator=(const Image& other)
+{
+	image = other.image;
+	PControl::operator=(other);
+	return *this;
+}
+
+void Image::setImage(ImageTexture image)
+{
 	this->image = image;
 
 	PFloatColor color = getColor();
@@ -30,19 +42,19 @@ void Image::setImage(SDL_Texture* image, bool free)
 	setAlpha(getAlpha());
 }
 
-void Image::setImage(std::string path, SDL_Renderer* ren, bool free)
+void Image::asyncSetImage(ImageRWops& imageRWops)
 {
-	if (free && this->image)
-		SDL_DestroyTexture(this->image);
-
-	this->image = IMG_LoadTexture(ren, path.c_str());
-
-	PFloatColor color = getColor();
-	setColor(color.r, color.g, color.b);
-	setAlpha(getAlpha());
+	++loadQueueCount;
+	createAsyncLoadThread(getNewThreadData("", imageRWops));
 }
 
-SDL_Texture* Image::getImage()
+void Image::asyncSetImage(std::string path)
+{
+	++loadQueueCount;
+	createAsyncLoadThread(getNewThreadData(path, ImageRWops{ nullptr, nullptr }));
+}
+
+ImageTexture Image::getImage() const
 {
 	return image;
 }
@@ -50,7 +62,7 @@ SDL_Texture* Image::getImage()
 void Image::freeImage()
 {
 	if (image)
-		SDL_DestroyTexture(image);
+		image.reset();
 }
 
 void Image::setX(float x)
@@ -84,7 +96,7 @@ void Image::setColor(float r, float g, float b)
 	if (image)
 	{
 		PIntColor color = getRoundedColor();
-		SDL_SetTextureColorMod(image, color.r, color.g, color.b);
+		SDL_SetTextureColorMod(image.get(), color.r, color.g, color.b);
 	}
 }
 
@@ -93,13 +105,163 @@ void Image::setAlpha(float a)
 	PControl::setAlpha(a);
 
 	if (image)
-		SDL_SetTextureAlphaMod(image, getRoundedAlpha());
+		SDL_SetTextureAlphaMod(image.get(), getRoundedAlpha());
 }
 
 void Image::draw(SDL_Renderer* ren)
 {
-	if (image)
-		SDL_RenderCopy(ren, image, nullptr, &dest);
+	draw(ren, nullptr);
+}
 
+void Image::draw(SDL_Renderer* ren, SDL_Rect* view)
+{
+	if (image)
+		SDL_RenderCopy(ren, image.get(), view, &dest);
+
+	pollImageBuffers(ren);
 	PControl::draw(ren);
+}
+
+ImageTexture Image::makeImageTexture(SDL_Texture* texture)
+{
+	return ImageTexture(texture, SDL_DestroyTexture);
+}
+
+
+ImageTextureThreadData* Image::getNewThreadData(std::string path, ImageRWops imageRWops)
+{
+	ThreadSurfaceBuffer* threadImageBuffer = new ThreadSurfaceBuffer();
+	threadImageBuffer->ready = false;
+
+	ImageTextureThreadData* threadData = new ImageTextureThreadData();
+	threadData->caller = this;
+	threadData->path = path;
+	threadData->imageRWops = imageRWops;
+	threadData->threadNumber = loadQueueCount;
+	threadData->buffer = threadImageBuffer;
+
+	return threadData;
+}
+
+void Image::createAsyncLoadThread(ImageTextureThreadData* threadData)
+{
+	if (SDL_LockMutex(mutex) == 0)
+	{
+		// Store a ThreadSurfaceBuffer in imageBuffers for the new thread to be made.
+		imageBuffers[threadData->threadNumber] = threadData->buffer;
+		SDL_UnlockMutex(mutex);
+	}
+	else
+		throw SDL_GetError();
+
+	SDL_Thread* thread = SDL_CreateThread(asyncLoadImage, nullptr, threadData);
+}
+
+int Image::asyncLoadImage(void* data)
+{
+	ImageTextureThreadData* threadData = static_cast<ImageTextureThreadData*>(data);
+	Image* caller = threadData->caller;
+
+	// Load texture if current thread is the last image to be loaded.
+	if (threadData->threadNumber == caller->loadQueueCount)
+	{
+		// Load surface from an RWops.
+		if (threadData->imageRWops.rwops)
+		{
+			assert(threadData->path.empty());
+
+			threadData->buffer->surface = IMG_Load_RW(threadData->imageRWops.rwops, 1);
+
+			// Delete the buffer if given.
+			if (threadData->imageRWops.buffer)
+				delete[] threadData->imageRWops.buffer;
+		}
+		// Load surface from a path.
+		else
+		{
+			assert(!threadData->imageRWops.buffer);
+
+			threadData->buffer->surface = IMG_Load(threadData->path.c_str());
+		}
+
+		// Recheck thread number, incase another image thread was created while loading.
+		if (threadData->buffer->surface && threadData->threadNumber == caller->loadQueueCount)
+			threadData->buffer->ready = true;
+		else
+		{
+			if (SDL_LockMutex(caller->mutex) == 0)
+			{
+				caller->imageBuffers.erase(threadData->threadNumber);
+				SDL_UnlockMutex(caller->mutex);
+			}
+			else
+				throw SDL_GetError();
+
+			if (threadData->buffer->surface)
+				SDL_FreeSurface(threadData->buffer->surface);
+
+			delete threadData->buffer;
+		}
+	}
+	else
+	{
+		if (SDL_LockMutex(caller->mutex) == 0)
+		{
+			caller->imageBuffers.erase(threadData->threadNumber);
+			SDL_UnlockMutex(caller->mutex);
+		}
+		else
+			throw SDL_GetError();
+
+		delete threadData->buffer;
+	}
+
+	delete threadData;
+
+	return 0;
+}
+
+void Image::pollImageBuffers(SDL_Renderer* ren)
+{
+	for (auto it = imageBuffers.begin(); it != imageBuffers.end();)
+	{
+		int threadNumber = it->first;
+		ThreadSurfaceBuffer* buffer = it->second;
+
+		// Load the latest surface from its respective buffer if it has finished loading.
+		if (threadNumber == loadQueueCount && buffer->ready)
+		{
+			ImageLoadReadyEvent er;
+			notify(&er);
+
+			// Convert the surface to a texture and set the internal image to it.
+			SDL_Texture* texture = SDL_CreateTextureFromSurface(ren, buffer->surface);
+			SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_BLEND);
+
+			setImage(makeImageTexture(texture));
+
+			int iw, ih;
+			SDL_QueryTexture(image.get(), nullptr, nullptr, &iw, &ih);
+
+			ImageLoadedEvent e(iw, ih);
+			notify(&e);
+		}
+
+		// Destroy all buffers that have finished loading and aren't needed anymore.
+		if (buffer->ready)
+		{
+			SDL_FreeSurface(buffer->surface);
+			delete buffer;
+
+			if (SDL_LockMutex(mutex) == 0)
+			{
+				it = imageBuffers.erase(it);
+				SDL_UnlockMutex(mutex);
+			}
+			else
+				throw SDL_GetError();
+		}
+		else
+			++it;
+	}
 }
--- pictualizer/controls/Image.h
+++ pictualizer/controls/Image.h
@@ -2,19 +2,62 @@
 
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_thread.h>
+#include <unordered_map>
+#include <memory>
 #include <string>
 #include "PControl.h"
+#include "../io/EventObserver.h"
 
-class Image : public PControl
+class Image;
+
+typedef std::shared_ptr<SDL_Texture> ImageTexture;
+
+/*
+ *	ThreadSurfaceBuffers are polled by the Image object to determine if asynchronously loaded images have finished loading.
+ */
+struct ThreadSurfaceBuffer
+{
+	SDL_Surface* surface;
+	bool ready;
+};
+
+/*
+ *	RWops sent to an Image contain dynamically allocated buffers that must be deleted after an RWops has been freed.
+ */
+struct ImageRWops
+{
+	SDL_RWops* rwops;
+	uint8_t* buffer;
+};
+
+/*
+ *  ImageTextureThreadData holds necessary data for Image threads to perform asynchronous loading.
+ */
+struct ImageTextureThreadData
+{
+	ThreadSurfaceBuffer* buffer;
+	int threadNumber;
+	Image* caller;
+
+	std::string path;
+	ImageRWops imageRWops;
+};
+
+class Image : public PControl, public EventObserver
 {
 	public:
-		Image(SDL_Texture* image, float x, float y, float w, float h);
+		Image(ImageTexture image, float x, float y, float w, float h);
 		Image(float x, float y, float w, float h);
 		~Image();
 
-		void setImage(SDL_Texture* image, bool free = true);
-		void setImage(std::string path, SDL_Renderer* ren, bool free = true); 
-		SDL_Texture* getImage();
+		Image(const Image& other);
+		Image& operator=(const Image& other);
+
+		void setImage(ImageTexture image);
+		void asyncSetImage(ImageRWops& imageRWops);
+		void asyncSetImage(std::string path);
+		ImageTexture getImage() const;
 
 		void freeImage();
 
@@ -27,8 +70,29 @@ class Image : public PControl
 		void setAlpha(float a);
 
 		void draw(SDL_Renderer* ren);
+		void draw(SDL_Renderer* ren, SDL_Rect* view);
+
+		/*
+		 *	Images use shared_ptrs of SDL_Textures internally to allow for textures to be freed automatically.
+		 *	This allows users to pass SDL_Textures to Image objects without having to free them manually.
+		 */
+		static ImageTexture makeImageTexture(SDL_Texture* texture);
+
+		/*
+		 *	Images must have their internal image buffers polled to properly load asynchronously loaded images. If an Image object's
+		 *	draw() method is not called regularly, then this method MUST be called each time before loading in a new image.
+		 */
+		void pollImageBuffers(SDL_Renderer* ren);
 
 	private:
-		SDL_Texture* image;
+		ImageTexture image;
 		SDL_Rect dest;
+
+		SDL_mutex* mutex;
+		std::unordered_map<int, ThreadSurfaceBuffer*> imageBuffers;
+		int loadQueueCount;
+
+		ImageTextureThreadData* getNewThreadData(std::string path, ImageRWops imageRWops);
+		void createAsyncLoadThread(ImageTextureThreadData* threadData);
+		static int asyncLoadImage(void* data);
 };
